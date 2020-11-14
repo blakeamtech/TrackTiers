7568f25f3e8cf5b06009a3ce23d8e7f0d3575289
Blake Martin <56556280+bamartin1618@users.noreply.github.com>
2020-11-14 13:12:06 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,33 +1,5 @@
 // Variables
 
-var albums = [
-  {"name": "Eternal Atake",
-  "artist": "Lil Uzi Vert",
-  "release": 2020,
-  "tracks": ["Baby Pluto","Lo Mein","Silly Watch","POP","You Better Move","Homecoming","I'm Sorry","Celebration Station","Bigger Than Life","Chrome Heart Tags","Bust Me","Prices","Urgency (feat. Syd)","Venetia","Secure The Bag","P2","Futsal Shuffle 2020 - Bonus Track", "That Way - Bonus Track"]
-  },
-  {"name": "Pluto x Baby Pluto",
-  "artist": "Future, Lil Uzi Vert",
-  "release": 2020,
-  "tracks": ["Stripes Like Burberry","Marni On Me","Sleeping On The Floor","Real Baby Pluto","Drankin And Smokin","Million Dollar Play","Plastic","That's It","Bought A Bad Bitch","Rockstar Chainz","Lullaby","She Never Been To Pluto","Off Dat","I Don't Wanna Break Up","Bank Roll","Moment Of Clarity"]
-  },
-  {"name": "The Goat",
-  "artist": "Polo G",
-  "release": 2020,
-  "tracks": ["Don't Believe The Hype","Heartless (feat. Mustard)","Martin & Gina","Flex (feat. Juice WRLD)","Go Stupid","21","33","I Know","Beautiful Pain (Losin My Mind)","No Matter What","Be Something (feat. Lil Baby)","Relentless","DND","Chinatown","Trials and Tribulations","Wishing For a Hero (feat. BJ The Chicago Kid)"]
-  },
-  {"name": "My Turn",
-  "artist": "Lil Baby",
-  "release": 2020,
-  "tracks": ["Get Ugly", "Heatin Up (feat. Gunna)", "How", "Grace (feat. 42 Dugg)", "Woah", "Live Off My Closet (feat. Future)", "Same Thing", "Emotionally Scarred", "Commercial (feat. Lil Uzi Vert)", "Forever (feat. Lil Wayne)", "Can't Explain", "No Sucker (feat. Moneybagg Yo)", "Sum 2 Prove", "We Should (feat. Young Thug)", "Catch The Sun - From \"Queen & Slim: The Soundtrack\"", "Consistent","Gang Signs","Hurtin","Forget That (feat. Rylo Rodriguez", "Solid"]
-  },
-  {"name": "Lil Uzi Vert vs. The World",
-  "artist": "Lil Uzi Vert",
-  "release": 2016,
-  "tracks": ["Canadian Goose","Hi Roller","Money Longer","Grab the Wheel","You Was Right","Baby Are You Home","Ps & Qs","Team Rocket","Scott and Ramona"]
-  }
-];
-
 
 
 const colorGrades = {
